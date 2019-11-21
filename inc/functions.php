d51cb656b8dd2f318e233ef60d398bd28dafec94
chbears3 <chbears3@gmail.com>
2019-11-20 23:54:30 -0500

    Base application

--- inc/functions.php
+++ inc/functions.php
@@ -1,12 +1,51 @@
+<?php
 // PHP - Random Quote Generator
 
 // Create the Multidimensional array of quote elements and name it quotes
 // Each inner array element should be an associative array
-
-
+$quotes = [
+    [
+        "quote"    => "Quote in arr 1",
+        "source"   => "Charles1",
+        "citation" => "citation",
+        "year"     => "year",
+    ],
+    [
+        "quote"    => "Quote in arr 2",
+        "source"   => "Charles2",
+        "citation" => "citation",
+        "year"     => "year",
+    ],
+    [
+        "quote"    => "Quote in arr 3",
+        "source"   => "Charles3",
+        "citation" => "citation",
+        "year"     => "year",
+    ],
+];
 
 // Create the getRandomQuuote function and name it getRandomQuote
-
+function getRandomQuote($quotes){
+    $rand_int = rand(0, 2);
+    return $quotes[$rand_int];
+}
 
 
 // Create the printQuote funtion and name it printQuote
+function printQuote($quotes) {
+    $quote     = getRandomQuote($quotes);
+    $quote_str = $quote["quote"];
+    $source    = $quote["source"];
+    $citation  = $quote["citation"];
+    $year      = $quote["year"];
+
+$quote_div = <<<END
+<div id="quote-box">
+    <p class="quote">$quote_str</p>
+    <p class="source">$source<span class="citation">$citation</span><span class="year">$year</span></p>
+</div>
+END;
+
+    return $quote_div;
+}
+?>
